ddca57c42bcf1e8305a0559050a16c417530e36e
Alumno <Alumno@ws204-10>
2019-02-06 18:12:14 -0800

    Cambio local

--- Paquetes/src/Pruebas/Test.java
+++ Paquetes/src/Pruebas/Test.java
@@ -1,14 +1,17 @@
 package Pruebas;
 
+import paquete1.Persona;
+
 public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		paquete1.Persona p = new paquete1.Persona(47,"Pepe");
 		paquete2.Persona m = new paquete2.Persona(24, "Penelope", "Femenino");
-		
+		paquete1.Persona n = new paquete1.Persona(37,"Juan");
 		System.out.println(p.toString());
 		System.out.println(m.toString());
+		System.out.println(n.toString());
 		
 	}
 
